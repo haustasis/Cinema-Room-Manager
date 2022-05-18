e8a20a7720e4397507f30c28de1a890310cc557c
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-05-18 10:05:38 +0200

    Stage 2/5: Sold!

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -1,14 +1,31 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
+        System.out.println("Enter the number of rows: ");
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of seats in each row: ");
+        int seats = sc.nextInt();
+        System.out.println("Total income: ");
+        System.out.println("$" + (calculateTotalIncome(rows, seats)));
+    }
 
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        for(int i = 1; i < 8; i++) {
-            System.out.println(i + " S S S S S S S S");
+    private static int calculateTotalIncome(int rows, int seats) {
+        int totalIncome;
+        if(rows * seats <= 60) {
+            totalIncome = rows * seats * 10;
+        } else {
+            if(rows % 2 == 0) {
+                totalIncome = (10 * seats * rows / 2) + (8 * seats * rows / 2);
+            } else {
+                totalIncome = ((10 * seats) * ((rows - 1) / 2)) + ((8 * seats) * ((rows + 1) / 2));
+            }
         }
+        return totalIncome;
     }
 }
